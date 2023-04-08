f92f1aaaa0172fff9296735b18bb4b06388e6454
RGM <64647695+rosagrandio@users.noreply.github.com>
2023-04-08 03:03:24 +0200

    Update model_loader.py

--- Deployment-DEV/service/model_loader.py
+++ Deployment-DEV/service/model_loader.py
@@ -7,8 +7,8 @@ import tensorflow as tf
 
 def loadModelH5():
 
-    MODEL_H5_FILE = "flowers_model_full_tf2.h5"
-    MODEL_H5_PATH = "../../../models/tf2x/keras/full/"
+    MODEL_H5_FILE = "modelo_pantera_rosa.h5"
+    MODEL_H5_PATH = "../../../modelo_pantera_rosa/"
 
     # Cargar el modelo DL desde disco
     loaded_model = tf.keras.models.load_model(MODEL_H5_PATH + MODEL_H5_FILE)
